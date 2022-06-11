64560a5eb7a04ae83a56d00e4bc0f12f8f605db1
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-06-10 22:14:13 -0400

    Added a for loop to make certain amount of boxs

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let nu = 256;
+let size =800;
+let amount = 4;
+
 let fox = document.getElementsByClassName("main");
 start();
-function start(){
-    let me = document.createElement("div");
+function start(){   
+    //Determines the required size of each box based off the amount
+    let width = size/amount;
+
+    for(let i =0;i<amount;i++){
+        let me = document.createElement("div");
+        me.classList.add("box");
+        me.style.backgroundColor = "red";
+        me.style.width=width+"px";
+        me.style.height = width+"px";
+        document.getElementById('main').appendChild(me);
+    }
+   
+
+
     
-    me.classList.add("box");
-    me.style.backgroundColor = "red";
-    document.getElementById('main').appendChild(me);
     
 }
