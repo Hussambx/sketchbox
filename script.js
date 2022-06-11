7ce034b7320730ef9126c4765fb8416055256b0e
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-06-10 22:59:58 -0400

    Fixed the math calculation for box dimensions

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let nu = 256;
-let size =800;
-let amount = 4;
+let size =800*800;
+let amount = 16;
 
 let fox = document.getElementsByClassName("main");
 start();
 function start(){   
     //Determines the required size of each box based off the amount
-    let width = size/amount;
+    let widthx = Math.sqrt((size/amount));
 
     for(let i =0;i<amount;i++){
         let me = document.createElement("div");
         me.classList.add("box");
         me.style.backgroundColor = "red";
-        me.style.width=width+"px";
-        me.style.height = width+"px";
+        me.style.width=widthx+"px";
+        me.style.height = widthx+"px";
         document.getElementById('main').appendChild(me);
     }
    
