254009d427bf8317e6aa090fec36091a076d4fc8
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-06-11 11:58:35 -0400

    Added event listenter to each grid box created

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let nu = 256;
-let size =800*800;
-let amount = 16;
+let size =800*800; //Area of play area
+let amount = 4*4;
 
 let fox = document.getElementsByClassName("main");
 start();
@@ -9,16 +9,29 @@ function start(){
     let widthx = Math.sqrt((size/amount));
 
     for(let i =0;i<amount;i++){
-        let me = document.createElement("div");
-        me.classList.add("box");
+        let me = document.createElement("section");
+        me.id ="box";
         me.style.backgroundColor = "red";
         me.style.width=widthx+"px";
         me.style.height = widthx+"px";
         document.getElementById('main').appendChild(me);
     }
+  
    
 
-
-    
-    
 }
+
+    // your code to run after the timeout
+    const buttons = document.querySelectorAll('section');
+buttons.forEach((section) => {
+
+    // and for each one we add a 'click' listener
+    section.addEventListener('mouseover',function (e) {
+        e.target.style.backgroundColor="blue";
+    });
+  });
+
+
+
+// stop for sometime if needed
+
